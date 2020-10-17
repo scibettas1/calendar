3bfd6c8879a3b23500346f3e415dc55c94140255
Sam Scibetta <sammieeccentric@gmail.com>
2020-10-17 16:07:19 -0400

    the css is good but none of the js works

--- script.js
+++ script.js
@@ -1,11 +1,35 @@
 
-//on click save to local storage
-localStorage.setItem("scores", JSON.stringify(high_scores));
-
-//prints back into the text box
-.val
-var high_scores = localStorage.getItem("scores");
+//variable(s)
 
 
+//add date and time to the top of the page THIS CODE DOES NOT WORK
+$("#currentDay").text.moment().format('MMMM Do YYYY, h:mm:ss a');
 
-//if else statemenmts for past present and future and change the color
+
+//on click save to local storage THIS CODE ALSO DOES NOT WORK
+$(".saveBtn").on("click", function () {
+    var task = $(this).siblings(".description").val();
+    var hour = $(this).parent(".time").attr("id");
+    localStorage.setItem(task, hour);
+    console.log(task)
+});
+
+//pull content from local storage
+
+$("#hour9").val(localStorage.getItem("hour9"));
+$("#hour10").val(localStorage.getItem("hour10"));
+$("#hour11").val(localStorage.getItem("hour11"));
+$("#hou12").val(localStorage.getItem("hour12"));
+$("#hour1").val(localStorage.getItem("hour1"));
+$("#hour2").val(localStorage.getItem("hour2"));
+$("#hour3").val(localStorage.getItem("hour3"));
+$("#hour4").val(localStorage.getItem("hour4"));
+$("#hour5").val(localStorage.getItem("hour5"));
+$("#hour6").val(localStorage.getItem("hour6"));
+$("#hour7").val(localStorage.getItem("hour7"));
+
+
+
+
+
+//3 if else statemenmts for past present and future and change the color
