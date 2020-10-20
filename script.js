82c10e04080fa382461d191c4c025870fa9ba1bd
Sam Scibetta <sammieeccentric@gmail.com>
2020-10-19 20:12:22 -0400

    created my if else statements and a few other changes

--- script.js
+++ script.js
@@ -1,28 +1,34 @@
+//add date and time to the top of the page
+setInterval(function () {
+    var time = moment().format('MMMM Do YYYY, h:mm:ss a');
+    $("#currentDay").text(time);
 
-//variable(s)
+    //console.log(time)
+}, 1000);
 
+// var hour9El = $("#hour9")
+// console.log(hour9El)
+// var elementHour = hour9El.data("hour")
+// console.log(elementHour)
 
-//add date and time to the top of the page THIS CODE DOES NOT WORK
-var time = moment().format('MMMM Do YYYY, h:mm a');
-$("#currentDay").text(time);
+//on click save to local storage
 
-console.log(time)
-
-//$("#currentDay").text.moment().format('MMMM Do YYYY, h:mm:ss a');
-
-
-//on click save to local storage THIS CODE ALSO DOES NOT WORK
 $(".saveBtn").on("click", function () {
     var task = $(this).siblings(".description").val();
-    var hour = $(this).parent(".time").attr("id");
-    localStorage.setItem(task, hour);
     console.log(task)
+
+    var time = $(this).parent().attr("id");
+
+    // saving to local storage
+    localStorage.setItem(time, task);
+    
 });
 
-//pull content from local storage I CAN'T TEST THIS BECAUSE EVRYTHING ELSE IS BROKEN
+//pull content from local storage 
+//I can't test this because code above is broken
 
-$("#hour9").val(localStorage.getItem("hour9"));
-$("#hour10").val(localStorage.getItem("hour10"));
+$("#9 .description").val(localStorage.getItem("9"));
+$("#10 .description").val(localStorage.getItem("10"));
 $("#hour11").val(localStorage.getItem("hour11"));
 $("#hou12").val(localStorage.getItem("hour12"));
 $("#hour1").val(localStorage.getItem("hour1"));
@@ -33,10 +39,46 @@ $("#hour5").val(localStorage.getItem("hour5"));
 $("#hour6").val(localStorage.getItem("hour6"));
 $("#hour7").val(localStorage.getItem("hour7"));
 
-
-
-
-
 //3 if else statemenmts for past present and future and change the color
-// I CAN'T EVEN BEGIN TO WRITE THIS BECAUSE EVERYTHING ELSE IS BROKEN
+// I don't know where to begin with this
 
+//if the value of the div is equal to h it should be a white
+
+//how do i check if the value of something is less than or greater than in refrence to time?
+//AM and PM throws a wrench in the whole thing
+
+
+
+//hourUpdater function
+function hourUpdater(){
+// get current number of hourse
+var currentHour = moment().format('H');
+
+    // loop over the time blocks .each
+$(".time-block").each(function(){
+var blockHour = parseInt($(this).attr("id"));
+
+if (blockHour < currentHour){
+    $(this).addClass("past")
+} else if (blockHour === currentHour){
+    $(this).removeClass("past")
+    $(this).addClass("present")
+} else {
+    $(this).removeClass("past")
+    $(this).removeClass("present")
+    $(this).addClass("future")
+}
+});
+
+hourUpdater();
+
+
+    // set variable for time block to pasteInt
+
+    // check to see if we have moved past the current time
+
+    // else if at current time
+
+    // else if not this time
+}
+    
